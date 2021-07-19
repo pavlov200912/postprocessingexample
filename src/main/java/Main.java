4d97da35a248c4462135cb78b08ab266fec30750
Ivan Pavlov <pavlov200912@gmail.com>
2021-07-19 21:13:42 +0300

    method changed 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ public class Main {
      * This is a demo function with changed comment
      * */
     public int demo(int first, int b, int c, int d) {
-        return first + b + c + first + b + c + d + d;
+        return first + b + c + first + b + c + d + d + first;
     }
 }
