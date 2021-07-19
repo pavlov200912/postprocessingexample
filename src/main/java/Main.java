2fd33154b896aa73ba522b08c1a425635bd32848
Ivan Pavlov <pavlov200912@gmail.com>
2021-07-19 21:12:39 +0300

    method change with param added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     /**
      * This is a demo function
      * */
-    public int demo(int a, int b, int c) {
-        return a + b + c + a + b + c;
+    public int demo(int a, int b, int c, int d) {
+        return a + b + c + a + b + c + d;
     }
 }
