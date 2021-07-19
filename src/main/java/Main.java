8a600312d3effca8c3661f9b570b48d5b6bcf00a
Ivan Pavlov <pavlov200912@gmail.com>
2021-07-19 21:45:35 +0300

    new comment code change, method comment code change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
     /**
-     * This is a demo function with changed comment, also comment changed twice, third comment change
+     * This is a demo function with changed comment, also comment changed twice, third comment change, fourth
      * */
     public int renamed_demo(int first, int b, int c, int d, int testing) {
-        return first + b + c + first + b + c + d + d + first + testing + first;
+        return first + b + c + first + b + c + d + d + first + testing;
     }
-    /** This is new method **/
+    /** This is new method with changed comment btw**/
     public double newMethod(double a, double b) {
-        return a + b + b;
+        return a + b + b + b;
     }
 }
