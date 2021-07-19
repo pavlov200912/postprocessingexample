877ffb9afd0428a38ce0ea5e2c8f5d4e3372002e
Ivan Pavlov <pavlov200912@gmail.com>
2021-07-19 21:14:56 +0300

    method change 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
      /**
      * This is a demo function with changed comment, also comment changed twice
      * */
-    public int demo(int first, int b, int c, int d, int testing) {
+    public int renamed_demo(int first, int b, int c, int d, int testing) {
         return first + b + c + first + b + c + d + d + first + testing + first + testing;
     }
 }
