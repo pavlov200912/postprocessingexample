48bcbfac8f2c638094cea4a62c2e94b1987542e9
Ivan Pavlov <pavlov200912@gmail.com>
2021-07-19 21:15:19 +0300

    comment change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     /**
-     /**
-     * This is a demo function with changed comment, also comment changed twice
+     * This is a demo function with changed comment, also comment changed twice, third comment change
      * */
     public int renamed_demo(int first, int b, int c, int d, int testing) {
         return first + b + c + first + b + c + d + d + first + testing + first + testing;
